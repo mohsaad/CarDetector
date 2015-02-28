17383d5e96080f8d3f65e4104e5c9bcf932ad0a6
mohsaad <msaad96@gmail.com>
2015-02-28 17:39:09 -0600

    finished reference image script

--- detect_space.py
+++ detect_space.py
@@ -3,7 +3,9 @@
 # Uses calibrate.png as our reference image. 
 # Uses 4-point perspective transform in OpenCV to check whether a car is
 # in the specified space or not
-# 4-point transform with assistance from Adrian Rosebrock
+# 4-point transform with assistance from Adrian Rosebrock (pyimagesearch.com)
+# http://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+
 import cv2
 import imutils
 import argparse
@@ -35,7 +37,7 @@ def loadCoords(filename):
 
 def order_points(pts):
 	# initialize array of zeros
-	rect = np.zeros(4,2),dtype = 'float32')
+	rect = np.zeros((4,2),dtype = 'float32')
 	
 	s = pts.sum(axis = 1)
 	rect[0] = pts[np.argmin(s)]
@@ -48,3 +50,51 @@ def order_points(pts):
 
 	return rect
 
+def persp_transform(image, pts):
+	# order points
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+
+	# get new width
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[0] - bl[0]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[0] - tl[0]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+
+	# get new height
+	heightA = np.sqrt(((tr[1] - br[1]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[1] - bl[1]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+
+	# create a bird's eye view of the image
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+
+	# compute transform
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+	# return
+	return warped
+
+
+
+# main method, runs every time
+def main():
+	img = cv2.imread("calibrate.png")
+	arr = loadCoords("coords.txt")
+	i = 0
+
+	for coords in arr:
+		pts = np.array(arr[i], dtype = "float32")
+		warped = persp_transform(img, pts)
+		cv2.imwrite(str(i) +".png", warped)
+
+
+	print "reference images calculated"
+	
+
+if __name__ == '__main__':
+	main()
