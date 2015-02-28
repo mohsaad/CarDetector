1166b8dcaa68e01900a414abb1411057c525c8f5
mohsaad <msaad96@gmail.com>
2015-02-28 11:25:28 -0600

    added calibration script

---
+++ calibrate.py
@@ -0,0 +1,28 @@
+# Mohammad Saad
+# 2/28/2015
+# TO use, put the camera in the spots you want to track, and then 
+# 
+
+
+import cv2
+import imutils
+
+
+
+def getImage(camera):
+	retval, im = camera.read()
+	return im
+
+
+def main():
+	camera_port = 0
+	cam = cv2.VideoCapture(camera_port)
+
+	img = getImage(cam)
+	cv2.imwrite("calibrate.png", img)
+	del(cam)
+
+
+if __name__ == '__main__':
+	main()
+
